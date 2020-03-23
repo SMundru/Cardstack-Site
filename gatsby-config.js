2fc46b1662bdeed266fa05dc370e9e9a46b386d9
Sivan Mundru <sivan.m@outlook.com>
2020-03-23 18:01:26 +0000

    change the bucket name.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-s3`,
       options: {
-        bucketName: "card-stack",
+        bucketName: "www.card-stack.co.uk",
       },
     },
   ],
